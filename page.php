ff7f1feb912e87fdda51fde3dc5156f9e8f2eab2
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-06-11 22:37:21 +0300

    add default theme

--- page.php
+++ page.php
@@ -13,16 +13,5 @@
  */
 
 get_header(); ?>
-
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				<?php
-				while ( have_posts() ) : the_post();
-				endwhile; // End of the loop.
-				?>
-			</div>
-		</div>
-	</div>
 <?php
 get_footer();
