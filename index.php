b9a9a7a0927f0ac479310b424b612dd4c618822d
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-08-26 21:04:33 +0300

    test

--- index.php
+++ index.php
@@ -10,3 +10,5 @@
 </style>
 <h1>this is Nuwera aggaa !!! </h1>
 <h2>Esheglar system!!!!</h2>
+
+<iframe src="https://www.instagram.com/reel/DLdQQdlNC22/embed" width="400" height="480" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
