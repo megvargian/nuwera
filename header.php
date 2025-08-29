76bc1f42d66f96cfba4d1087997398aeb8d6636c
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-08-29 21:13:25 +0300

    changes

--- header.php
+++ header.php
@@ -15,6 +15,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Nuwera - Heavy Metal Band</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="https://nuwera.band/wp-content/uploads/2024/09/cropped-Nuwera-Logo-White-192x192.png" sizes="192x192">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
@@ -22,3 +25,8 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site main_page_wrapper">
 	<div id="content" class="site-content">
+	<header style="text-align:center; padding:30px 0; background:#1b1b1b;">
+        <a href="/" style="display:inline-block;">
+            <img src="https://nuwera.band/wp-content/uploads/2024/09/Nuwera-Name-Only-White-500x132.png" alt="Nuwera Logo" style="max-width:300px; width:100%; height:auto;">
+        </a>
+    </header>
