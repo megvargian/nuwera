85c2d578ca0930e9c26d8c1ac1c7e2250314a941
Charlie-A77 <charlie.aintablian@barid.org>
2025-08-30 20:55:01 +0300

    aug-30-1

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 	<title>Nuwera - Heavy Metal Band</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="https://nuwera.band/wp-content/uploads/2024/09/cropped-Nuwera-Logo-White-192x192.png" sizes="192x192">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
