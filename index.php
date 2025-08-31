b4ab3229229bfdef544516c6be08ff1efb6d8c0f
Charlie-A77 <charlie.aintablian@barid.org>
2025-08-31 19:29:13 +0300

    latest single font-17

--- index.php
+++ index.php
@@ -13,10 +13,11 @@ get_header();
     </section>
 
     <section id="latest-single" style="text-align:center; background:#1b1b1b;">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-6">
                     <img src="https://nuwera.band/wp-content/uploads/2024/09/Groundless-Square-375x375.jpg" alt="Groundless" style="max-width:375px; width:100%; height:auto; margin:30px 0;">
+                    <img src="<?php echo get_template_directory_uri();?>/inc/assets/images/vinyl.png">
                 </div>
                 <div class="col-6">
                     <h2 style="color:#ff2b59; font-family:'Albert Extrabold',sans-serif;">LATEST SINGLE</h2>
