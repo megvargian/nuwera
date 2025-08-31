aa2f2aa7210ac8ed743d539e2f71d2d63274a47f
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-08-31 11:51:49 +0300

    add member section

--- index.php
+++ index.php
@@ -151,35 +151,35 @@ get_header();
             <div class="row gap-x-4">
                 <div class="col">
                     <a href="#" class="band-member">
-                        <img src="https://nuwera.band/wp-content/uploads/2024/10/MIKE3758-1536x1024.jpg" alt="Denyo">
+                        <img class="w-100 d-block" src="https://nuwera.band/wp-content/uploads/2024/10/MIKE3758-1536x1024.jpg" alt="Denyo">
                         <h3>Denyo</h3>
                         <p>Vocals/Thythm</p>
                     </a>
                 </div>
                 <div class="col">
                     <a href="#" class="band-member">
-                        <img src="https://nuwera.band/wp-content/uploads/2024/10/MIKE3301-1536x1024.jpg" alt="Serge Achkarian">
+                        <img class="w-100 d-block" src="https://nuwera.band/wp-content/uploads/2024/10/MIKE3301-1536x1024.jpg" alt="Serge Achkarian">
                         <h3>Serge Achkarian</h3>
                         <p>Bassist</p>
                     </a>
                 </div>
                 <div class="col">
                     <a href="#" class="band-member">
-                        <img src="https://nuwera.band/wp-content/uploads/2024/10/MIKE4611-1536x1024.jpg" alt="Antoine Bayram">
+                        <img class="w-100 d-block" src="https://nuwera.band/wp-content/uploads/2024/10/MIKE4611-1536x1024.jpg" alt="Antoine Bayram">
                         <h3>Antoine Bayram</h3>
                         <p>Drummer</p>
                     </a>
                 </div>
                 <div class="col">
                     <a href="#" class="band-member">
-                        <img src="https://nuwera.band/wp-content/uploads/2024/10/Angelo-Center-1536x1254.jpg" alt="Angelo Zeidan">
+                        <img class="w-100 d-block" src="https://nuwera.band/wp-content/uploads/2024/10/Angelo-Center-1536x1254.jpg" alt="Angelo Zeidan">
                         <h3>Angelo Zeidan</h3>
                         <p>Lead Guitar</p>
                     </a>
                 </div>
                 <div class="col">
                     <a href="#" class="band-member">
-                        <img src="https://nuwera.band/wp-content/uploads/2024/10/MIKE4328-1536x1024.jpg" alt="Member 5">
+                        <img class="w-100 d-block" src="https://nuwera.band/wp-content/uploads/2024/10/MIKE4328-1536x1024.jpg" alt="Member 5">
                         <h3>Rick Garabedian</h3>
                         <p>Synth/Keys</p>
                     </a>
