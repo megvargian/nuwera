1201a30f77b408270e103daafb547ece5c9a56f1
Charlie-A77 <charlie.aintablian@barid.org>
2025-09-13 20:30:10 +0300

    12-sept-1-15

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ get_header();
 <main id="main" class="site-main">
     <section id="home" style="text-align:center;">
         <img id="nuwera-logo"src="<?php echo get_template_directory_uri();?>/inc/assets/images/Nuwera-Name-Only-White-500x132.webp" alt="Nuwera Logo">
-        <img class="d-none d-lg-block" id="nuwera-crowd-img" src="<?php echo get_template_directory_uri();?>/inc/assets/images/Nuwera-main-image.webp">
+        <img class="d-none d-md-block" id="nuwera-crowd-img" src="<?php echo get_template_directory_uri();?>/inc/assets/images/Nuwera-main-image.webp">
     </section>
 
     <section id="latest-single" class="align-content-center text-center py-5">
