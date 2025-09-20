1aa44cdb05a2289e967a744c7ee68dfa0897e594
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-09-20 22:43:54 +0300

    fix page generale

--- page.php
+++ page.php
@@ -12,5 +12,12 @@
  * @package WP_Bootstrap_Starter
  */
 get_header(); ?>
+<div class="container">
+	<?php
+	while ( have_posts() ) : the_post();
+		the_content();
+	endwhile; // End of the loop.
+	?>
+</div>
 <?php
 get_footer();
