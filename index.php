6f3ef98a47e3f52162ad67ede628db4d58dd2bc0
Charlie-A7 <charlieaintablian@gmail.com>
2026-01-09 22:00:21 +0200

    individual-song

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ get_header();
                 <div class="col-12 d-lg-none">
                     <h2>latest single</h2>
                     <h3 class="mb-4">god complex</h3>
-                    <p style="color:#ebe2d0;">Nuwera's Latest Single “God Complex” released on June 2023.</p>
+                    <p style="color:#ebe2d0;">Nuwera's Latest Single “God Complex” released on January 2025.</p>
                 </div>
                 <div class="col-12 col-lg-6">
                     <div class="d-flex align-items-center justify-content-center justify-content-lg-start h-100 w-100">
