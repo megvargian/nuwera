bfeb3a6cd078b36300e6620fb8f9ad3b9a28bac7
Charlie-A7 <charlieaintablian@gmail.com>
2026-01-09 21:43:52 +0200

    individual-song

--- single-release.php
+++ single-release.php
@@ -87,7 +87,7 @@ $get_all_fields = get_fields();
             </div>
         </div>
     </div>
-    <div class="row d-flex justify-content-center mt-5 single-release-product">
+    <div class="row d-flex justify-content-center mt-5 single-release-product ">
         <!-- <div class="col-12">
             <div class="d-flex flex-column align-items-center ">
                 <h3 class="p-5 text-white text-center">Learn how to play <?php the_title(); ?> Here:</h3>
@@ -110,7 +110,7 @@ $get_all_fields = get_fields();
                     while ($products->have_posts()) : $products->the_post();
                         global $product;
                         ?>
-        <div class="col-12 col-md-4 mb-4 mb-md-0 ">
+        <div class="col-12 col-md-6 mb-4 mb-md-0 d-flex justify-content-center">
             <a href="<?php echo get_permalink(); ?>" class="shop-product-card">
                 <div class="product-image-wrapper">
                     <?php if (has_post_thumbnail()) : ?>
